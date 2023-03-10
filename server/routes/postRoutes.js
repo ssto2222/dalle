70c1b6be49434dbdd86d4ab4cb6ed2bd6fbc9da2
tuttofare <tech2metail@gmail.com>
2023-03-11 02:16:22 +0900

    first commit

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,56 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
+
+import Post from "../mongodb/models/post.js";
+
+dotenv.config();
+
+const router = express.Router();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true
+});
+
+//GET ALL POST
+router.route('/').get(async (req, res) => {
+    try {
+        const posts = await Post.find({});
+
+        res.status(200).json({ success: true, data: posts });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error });
+    }
+})
+
+//create A POST
+router.route('/').post(async (req, res) => {
+    //try {
+    const { name, prompt, photo } = req.body;
+    const options = {
+        use_filename: true,
+        unique_filename: false,
+        overwrite: true,
+    };
+
+    try {
+        const photoUrl = await cloudinary.uploader.upload(photo, options);
+        const newPost = await Post.create({
+            name,
+            prompt,
+            photo: photoUrl.url,
+        });
+
+        res.status(200).json({ success: true, data: { newPost } });
+    }
+
+    catch (err) {
+        res.status(500).json({ success: false, message: 'Unable to create a post, please try again' });
+    }
+});
+
+
+export default router;
